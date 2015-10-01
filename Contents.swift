66059318615442dad64d778e71456ee71e74248f
titian1012 <cramos762@elab-m-02.mchenry.edu>
2015-10-01 15:00:17 -0500

    pick
    pick

---
+++ Contents.swift
@@ -0,0 +1,84 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var choice1 = 1
+var choice2 = 1
+var choice3 = 1
+var choice4 = 1
+var choice5 = 1
+var choice6 = 1
+
+println("You crash land just off on an island that seemed to be off the raidar when you were up in the air. You're in relatively shallow water. You may be able to search the plane for supplies. It's a 20 minute swim to land. You did however pick up a flare that was floating in the water and you have a swiss army knife in your pocket")
+println("1) Swim to land")
+println("2) Search the plane")
+
+if choice1 == 1 {
+    println("You reach land after 20 minutes. It seems to be a relatively large and lush island. Judging by the suns position it is roughly 6 pm. You see some palm trees and driftwood nearby on the beach. To your right you see a cave.")
+    println("1) Gather things to build a shelter on the beach")
+    println("2) Go to cave")
+    
+        if choice2 == 1 {
+            println("You go to the palm trees to gather their leaves to build a shelter. The trees are on the edge of what seems like a rainforest. While you are gathering its leaves you hear a faint cry for help. It seems to be deep in the jungle. It could be a survivor. However, you do not know if this island is inhabited.")
+            println("1) Find out where the cry is coming from")
+            println("2) Finish gathering things and build your shelter")
+            
+            if choice3 == 1 {
+                println("You decide to go into the forest to see who was crying for help. there is about 30 minutes of daylight left and it gets darker the farther you go into the rain forest. You hear the cry again and this time it sounds like its just a couple more meters. You start to feel like you are being watched. You see a figure on the ground moaning up ahead.")
+                println("1) Keep going and find out what or who the shape is")
+                println("2) Turn around and run back to the beach before it gets too dark in the forest")
+                
+                if choice4 == 1 {
+                    println("You get to what you thought was a body but instead you see a wild pig with a spear in its side. You hear a low mumbling behind you and you see the brush moving. the beaches direction is blocked by the unknown. to your left you hear what sounds like water flowing and to your right you can faintly see a peak over the canopy")
+                    println("1) Run toward the sound of water")
+                    println("2) Run toward the peak")
+                    
+                    if choice5 == 1 {
+                        println("You quickly jolt toward the sound of water. You can hear footsteps close behind you above the sound of you panting. As you get closer to the sound of the water you start to see lights flickering")
+                        println("1) Keep running towards the lights")
+                        println("2) Turn around and try to face whatever is chasing you")
+                        
+                        if choice6 == 1 {
+                            println("You finally break out of the forest. As you look around you see that you are in a big round area that seemed to be cleared of trees. You walk to the middle and you see a big round hole surrounded by torches. Inside the hole you see many skeletons. However you see 4 people you had been on the plane with each with spears and arrows in various places. You hear the things that were chasing you getting closer. You turn around and a spear is flying right at you. To be continued...")
+                        }
+                        
+                        if choice6 == 2 {
+                            println("You turn around and get down low into some brush. Through the brush you can see three figures. One is holding a spear and the other two are holding bows. you have no where left to go. if you try and run they will shoot you. You find a rock next to you. It's dark out now, almost to the point of where you can't see. You throw the rock as far as you can behind the three figures and make a run for it. To be continued...")
+                        }
+                    }
+                    
+                    if choice5 == 2 {
+                        println("You run in the direction of the peak. The forest seems to be ending. You eventually reach the end of the forest. Directly in the path of the peak you see massive pyramids. What seems to have been a home to thousands is odly abandoned. the complex seems big enough to hide in from the things that are chasing you. If you reach the peak the higher ground could come in handy to shoot that flair off of.")
+                        println("1) Hide in the ruins")
+                        println("2) Keep running for the peak")
+                        
+                        if choice6 == 1 {
+                            println("You see a near by pyramid with a room on top. You're far enough ahead that they won't see you climb up to get to the room. As you're running up the stairs you hear the things break out of the jungle and in a burst of panic you slip and some rocks fall beneath your feat making a large noise. You reach the top of the pyramid but it is too late. They surround you on all sides. To be continued...")
+                        }
+                        
+                        if choice6 == 2 {
+                            println("You keep on heading toward the peak. It seems to be a promising idea. About a half mile ahead you see the base of the peak. Hope seems near. Just as you get near the peak, you fail to realize that the land ends and it turns into a massive cliff. You try to stop but you can't. To be continued...")
+                        }
+                    }
+                }
+                
+                if choice4 == 2 {
+                    println("Driven by fear, you turn around and head back towards the beach. you fail to see a brilliantly laid trap. You step on what seems to be a rope, like a trip wire. This causes a swinging wall of spikes to hurdle towards you. You have no time to react. You're gone.")
+                    println("Thank you for playing")
+                }
+            }
+            
+            if choice3 == 2 {
+                println("You continue to gather the things necessary to build your shelter. you go back and find a spot close to the edge of the forest to set up camp. It takes you until it is dark to finish. You begin to get hungry. You know how to fish but have no fishing gear. You saw some coconuts by the trees earlier but you also remember seeing food on in the plane wreck.")
+                println("1) Gather coconuts and return back to your shelter quickly")
+                println("2) Swim out to the crash site and look for the food")
+            }
+    }
+        if choice2 == 2 {
+        //this is getting too long but you get the idea
+    }
+}
+
+if choice1 == 2 {
+    //this is getting too long but you get the idea
+}
